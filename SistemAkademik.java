574db8e3f61629117a46990c9ce0ecd26c016ee7
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2023-10-14 15:04:24 +0700

    menambahkan fitur 3 attempts utk login, logout, contact

--- SistemAkademik.java
+++ SistemAkademik.java
@@ -1,50 +1,63 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.io.IOException;
 
 public class SistemAkademik{
     // deklarasi variable global
     // private static int levelAcc;
     private static Scanner inputScanner = new Scanner(System.in);
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
+        logIn();
+    }
+
+    public static void logOut(){
+        System.out.println("Anda telah logout");
+        // Redirect user to login page
         logIn();
     }
     // private static List<String> menuStack = new ArrayList<>();
     public static void logIn(){
         String username, password;
-        int levelAcc;
-
-        System.out.println("Login User");
+        int attempts = 0;
+       
+    while(attempts < 3){
+        System.out.println("");
+        System.out.println("======================");
+        System.out.println("=     Login User     =");
+        System.out.println("=--------------------=");
         System.out.print("Masukkan username : ");
         username = inputScanner.next();
         System.out.print("Masukkan password : ");
         password = inputScanner.next();
 
         if ("admin".equals(username) && "admin".equals(password)) {
-            levelAcc = 1;
             System.out.println("Selamat datang " + username);
-            System.out.println("level" + levelAcc);
             berandaAdmin();
             
         }else if("dosen".equals(username) && "dosen".equals(password)){
-            levelAcc = 2;
             System.out.println("Selamat datang " + username);
-            System.out.println("level" + levelAcc);
             berandaDosen();
         }else if("mahasiswa".equals(username) && "mahasiswa".equals(password)){
-            levelAcc = 3;
             System.out.println("Selamat datang " + username);
-            System.out.println("level" + levelAcc);
             berandaMahasiswa();
         }else{
             System.out.println("username/password salah");
+            attempts++;
+        }
+    }
+        if (attempts == 3) {
+            System.out.println("Anda telah melebihi batas upaya login");
+            System.exit(0);
         }
     }
 
     private static void berandaAdmin(){
         while (true) {
-            System.out.println("Sistem Akademik");
+            System.out.println("");
+            System.out.println("============================");
+            System.out.println("=      Sistem Akademik     =");
+            System.out.println("=       BERANDA ADMIN      =");
+            System.out.println("----------------------------");
             System.out.println("1. Input Data Mahasiswa");
             System.out.println("2. Update Data Mahasiswa");
             System.out.println("3. Cek Data Mahasiswa");
@@ -54,7 +67,7 @@ public class SistemAkademik{
             System.out.println("7. Update KRS Mahasiswa");
             System.out.println("8. Cek Data KRS Mahasiswa");
             System.out.println("9. Contact");
-            System.out.println("0. Exit\n");
+            System.out.println("0. LogOut\n");
 
             System.out.print("Masukkan pilihan anda: ");
             int choice = inputScanner.nextInt();
@@ -84,10 +97,10 @@ public class SistemAkademik{
                     cekDataKrsMhs();
                     break;
                 case 9:
-                    contact();
+                    contactAdmin();
                     break;
                 case 0:
-                    return;
+                    logOut();
                 default:
                     System.out.println("Pilihan tidak valid.");
             }
@@ -95,21 +108,76 @@ public class SistemAkademik{
     }
 
     private static void berandaDosen(){
-        System.out.println("Sistem Akademik");
+         while (true) {
+        System.out.println("");
+        System.out.println("============================");
+        System.out.println("=      Sistem Akademik     =");
+        System.out.println("=       BERANDA DOSEN      =");
+        System.out.println("----------------------------");
         System.out.println("1. Cek Data Mahasiswa");
         System.out.println("2. Input Nilai Mahasiswa");
         System.out.println("3. Update Nilai Mahasiswa");
-        System.out.println("4. Update Nilai Mahasiswa");
-        System.out.println("0. Back");
+        System.out.println("4. Contact");
+        System.out.println("0. LogOut\n");
+
+         System.out.print("Masukkan pilihan anda: ");
+            int choice = inputScanner.nextInt();
+            switch (choice) {
+                case 1:
+                    cekDataMhs();
+                    break;
+                case 2:
+                    inNilaiMhs();
+                    break;
+                case 3:
+                    updNilaiMhs();
+                    break;
+                case 4:
+                    contact();
+                    break;
+                case 0:
+                    logOut();
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        }
     }
 
     private static void berandaMahasiswa(){
-        System.out.println("Sistem Akademik");
+
+        while (true) {
+        System.out.println("");
+        System.out.println("============================");
+        System.out.println("=      Sistem Akademik     =");
+        System.out.println("=     BERANDA MAHASISWA    =");
+        System.out.println("----------------------------");    
         System.out.println("1. Cek Data Mahasiswa");
         System.out.println("2. Cek Jadwal Kuliah");
         System.out.println("3. Cek Data KRS Mahasiswa");
         System.out.println("4. Contact");
-        System.out.println("0. Back");
+        System.out.println("0. LogOut\n");
+
+         System.out.print("Masukkan pilihan anda: ");
+            int choice = inputScanner.nextInt();
+            switch (choice) {
+                case 1:
+                    cekDataMhs();
+                    break;
+                case 2:
+                    cekJadwalKuliah();
+                    break;
+                case 3:
+                    cekDataKrsMhs();
+                    break;
+                case 4:
+                    contact();
+                    break;
+                case 0:
+                    logOut();
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        }
     } 
 
     // FITUR INPUT DATA MAHASISWA
@@ -225,18 +293,43 @@ public class SistemAkademik{
             }
         }
     }
-    private static void contact() {
+
+    // deklarasi variable global untuk fitur contact
+    public static String nama, pesan;
+    public static int nim;
+
+    public static void contact() {
         while(true){
         System.out.println("CONTACT");
-        System.out.println("1.");
-        System.out.println("2.");
-         int choice = inputScanner.nextInt();
-            switch (choice) {
-                case 0:
-                    return;
-                default:
-                    System.out.println("Pilihan tidak valid.");
-            }
+        System.out.print("masukkan nama : ");
+        nama = inputScanner.next();
+        System.out.print("masukkan nim : ");
+        nim = inputScanner.nextInt();
+        System.out.print("masukkan pesan : ");
+        pesan = inputScanner.next();
+
+        if (nama != "" && nim != 0 && pesan!="") {
+            System.out.println("Pesan anda sedang diproses");
+            return;
+        }
+        else{
+            System.out.println("pastikan data yang anda masukkan sudah benar");
+        }
+        //  int choice = inputScanner.nextInt();
+        //     switch (choice) {
+        //         case 0:
+        //             return;
+        //         default:
+        //             System.out.println("Pilihan tidak valid.");
+        //     }
         }
     }
+    public static void contactAdmin() {
+        System.out.println("CONTACT");
+        System.out.println("nama : " + nama);
+        System.out.println("nim : " + nim);
+        System.out.println("pesan : " + pesan);
+
+    }
+    
 }
